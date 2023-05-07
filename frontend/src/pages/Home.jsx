b71b965eaed730f642e59d984adab7e4b4dfa365
ricocatford <s7funoob@gmail.com>
2023-05-07 17:25:08 +0100

    feat:  form for sending recipes to DB

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,62 @@
+
+import React, { useState, useEffect } from "react";
+
+import getRecipes from "../requests/getRecipes";
+
 const Home = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+    async function requestGetRecipes() {
+        const recipes = await getRecipes();
+        setRecipes(recipes);
+    }
+    requestGetRecipes();
+    }, []);
+
+    const [recipeTitle, setRecipeTitle] = useState("")
+    const [recipeDescription, setRecipeDescription] = useState("")
+
+    const Test = () => {
+        fetch('http://localhost:8080/api/recipes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "title": recipeTitle,
+                "description": recipeDescription
+            })
+        })
+            .then(response => console.log(response.status));
+    }
+
+
+
     return (
-        <>
-            <p>
-                
-            </p>
-        </>
-    )
-}
+    <>
+        {recipes.map((recipe) => (
+            <div key={recipe.id}>
+                <p>{recipe.title}</p>
+                <p>{recipe.description}</p>
+            </div>
+        ))}
+        <form onSubmit={Test}>
+            <label htmlFor="title">
+                Title
+            </label>
+            <input type="text" value={recipeTitle} name="title" onChange={(e) => setRecipeTitle(e.target.value)}/>
+
+            <label htmlFor="description">
+                Description
+            </label>
+            <input type="text" value={recipeDescription} name="description" onChange={(e) => setRecipeDescription(e.target.value)}/>
+            <button type="submit">
+                Submit
+            </button>
+        </form>
+    </>
+    );
+};
+
+export default Home;
