250beb9bac742b43d8a19382086b0b5b18a551e1
Rico Catford <s7funoob@gmail.com>
2023-12-04 12:51:54 +0100

    fix(new recipe): refresh page on submit

--- frontend/src/components/RecipeForm.jsx
+++ frontend/src/components/RecipeForm.jsx
@@ -90,7 +90,7 @@ export default function RecipeForm({
             }
         });
         if (recipeFormMode !== "EDIT") {
-            event.currentTarget.submit();
+            window.location.reload();
         }
     };
 
